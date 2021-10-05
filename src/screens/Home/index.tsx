102f77ff46057141682ddf59f9db8ee7b2ed3e47
Marcos L <marcosrlj@outlook.com>
2021-10-05 03:10:41 -0300

    criacao das interfaces
    criacao do projeto e dica ninja

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {
+  Container,
+  Title
+} from './styles';
+
+// interface HomeProps {
+// }
+
+export const Home = () => {
+  return (
+    <Container>
+      <Title>Home</Title>
+    </Container>
+  );
+};
