9d4ed0dbd225e1426666c0ac84939d6d200d049e
Marcos L <marcosrlj@outlook.com>
2021-10-21 22:27:42 -0300

    obtendo a imagem em foco

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { FlatList } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { Dimensions, FlatList, NativeScrollEvent, NativeSyntheticEvent, ViewToken } from 'react-native';
 
 import {
   Container,
@@ -14,12 +14,35 @@ interface CarouselProps {
   imagesUrls: string[];
 }
 
+interface onViewableItemsChangedProps {
+  viewableItems: ViewToken[];
+  changed: ViewToken[]
+}
+
 export const Carousel = ({ imagesUrls }: CarouselProps) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0)
+
+  const handleOnViewableItemsChanged = useRef(((info: onViewableItemsChangedProps) => {
+    // console.log(info);
+    const index = info.viewableItems[0].index!
+    setCurrentImageIndex(index)
+  }))
+
+  // // my way: using onScroll
+  // const handleOnScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+  //   // use X offset with width to figure out which image is being displayed
+  //   const { width } = Dimensions.get('window')
+  //   console.log(event.nativeEvent.contentOffset);
+  //   const currentIndex = Math.round(event.nativeEvent.contentOffset.x / width)
+
+  //   setCurrentImageIndex(currentIndex)
+  // }
+
   return (
     <Container>
       <Indexes>
-        {imagesUrls.map(image => (
-          <ImageIndex key={image} active />
+        {imagesUrls.map((image, index) => (
+          <ImageIndex key={image} active={index === currentImageIndex} />
         ))}
       </Indexes>
 
@@ -34,6 +57,9 @@ export const Carousel = ({ imagesUrls }: CarouselProps) => {
           )}
           horizontal
           showsHorizontalScrollIndicator={false}
+          pagingEnabled
+          // onScroll={e => handleOnScroll(e)}
+          onViewableItemsChanged={handleOnViewableItemsChanged.current}
         />
     </Container>
   )
