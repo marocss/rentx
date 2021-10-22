22873f3464ada1a60cc003198244ee9da7ca9bd6
Marcos L <marcosrlj@outlook.com>
2021-10-21 23:11:54 -0300

    react native reanimated
    primeira animacao

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Button, StyleSheet } from 'react-native';
+import Animated, { useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
 
 import {
   Container,
@@ -8,9 +10,32 @@ import {
 // }
 
 export const Splash = () => {
+  const animation = useSharedValue(0)
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{translateX: animation.value}]
+    }
+  })
+  
+  const handleButton = () => {
+    animation.value += 10
+  }
 
   return (
     <Container>
+      <Animated.View style={[styles.box, animatedStyles]}>
+
+      </Animated.View>
+
+      <Button title={"move"} onPress={handleButton} />
     </Container>
   )
 }
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'orange'
+  }
+})
