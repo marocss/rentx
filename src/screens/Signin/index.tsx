ae6dd70956b8c8c481ff94b41aaa714d8d0c4b41
Marcos L <marcosrlj@outlook.com>
2021-11-02 09:54:29 -0300

    acessibilidade em formulario

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import {
+  KeyboardAvoidingView, StatusBar, Pressable, Keyboard,
+} from 'react-native';
 import { useTheme } from 'styled-components';
 import { Button } from '../../components/Button';
 import Input from '../../components/Input';
@@ -12,49 +14,54 @@ export const Signin = () => {
   const theme = useTheme();
 
   return (
-    <Container>
-      <StatusBar
-        barStyle="dark-content"
-        translucent
-        backgroundColor="transparent"
-      />
+    <KeyboardAvoidingView behavior="position" enabled>
+      <Pressable onPress={Keyboard.dismiss}>
+        <Container>
+          <StatusBar
+            barStyle="dark-content"
+            translucent
+            backgroundColor="transparent"
+          />
 
-      <Header>
-        <Title>
-          Almost
-          {'\n'}
-          there.
-        </Title>
-        <Subtitle>
-          Log in to start having
-          {'\n'}
-          an amazing experience.
-        </Subtitle>
-      </Header>
+          <Header>
+            <Title>
+              Almost
+              {'\n'}
+              there.
+            </Title>
+            <Subtitle>
+              Log in to start having
+              {'\n'}
+              an amazing experience.
+            </Subtitle>
+          </Header>
 
-      <InputSection>
-        <Input iconName="mail" placeholder="Email" keyboardType="email-address" />
+          <InputSection>
+            <Input iconName="mail" placeholder="Email" keyboardType="email-address" />
 
-        <Input
-          iconName="lock"
-          placeholder="Password"
-          keyboardType="visible-password"
-          secureTextEntry
-          isPassword
-        />
-      </InputSection>
+            <Input
+              iconName="lock"
+              placeholder="Password"
+              keyboardType="visible-password"
+              secureTextEntry
+              isPassword
+            />
+          </InputSection>
 
-      <Footer>
-        <Button title="Login" onPress={() => {}} disabled isLoading={false} />
-        <Button
-          title="Create an account"
-          onPress={() => {}}
-          isLoading={false}
-          color={theme.colors.background_two}
-          isLightBackground
-        />
-      </Footer>
+          <Footer>
+            <Button title="Login" onPress={() => {}} disabled isLoading={false} />
+            <Button
+              title="Create an account"
+              onPress={() => {}}
+              isLoading={false}
+              color={theme.colors.background_two}
+              isLightBackground
+            />
+          </Footer>
+        </Container>
+      </Pressable>
 
-    </Container>
+    </KeyboardAvoidingView>
+
   );
 };
--- src/screens/Signin/styles.ts
+++ src/screens/Signin/styles.ts
@@ -2,7 +2,7 @@ import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  flex: 1;
+  /* flex: 1; */
   padding: 0 24px;
   background-color: ${({ theme }) => theme.colors.background_one};
 `;
