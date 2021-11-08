fe3323f3d24e54aded3ba1fb2ca2f85fef014d73
Marcos L <marcosrlj@outlook.com>
2021-11-08 17:58:55 -0300

    estrategia de login
    backend do app
    cadastro de usuario

--- src/screens/SignUp/SignUpStepTwo/index.tsx
+++ src/screens/SignUp/SignUpStepTwo/index.tsx
@@ -1,8 +1,8 @@
-// import { useRoute } from '@react-navigation/native';
-import { useNavigation } from '@react-navigation/native';
+import { useRoute, useNavigation } from '@react-navigation/native';
 import React, { useState } from 'react';
 import { Alert, Keyboard, Pressable } from 'react-native';
 import { useTheme } from 'styled-components';
+import { api } from '../../../services/api';
 import { BackButton } from '../../../components/BackButton';
 import BulletIndicator from '../../../components/BulletIndicator';
 import { Button } from '../../../components/Button';
@@ -18,11 +18,13 @@ import {
   InputSection,
 } from './styles';
 
-// interface SignUpStepTwoRouteParams {
-//   name: string;
-//   email: string;
-//   driversLicense: string;
-// }
+interface SignUpStepTwoRouteParams {
+  user: {
+    name: string;
+    email: string;
+    driversLicense: string;
+  }
+}
 
 const SignUpStepTwo = () => {
   // eslint-disable-next-line no-unused-vars
@@ -31,14 +33,14 @@ const SignUpStepTwo = () => {
   const [passwordConfirmation, setPasswordConfirmation] = useState('');
 
   const { navigate } = useNavigation();
-  // const route = useRoute();
-  // const { user } = route.params as SignUpStepTwoRouteParams;
+  const route = useRoute();
+  const { user } = route.params as SignUpStepTwoRouteParams;
   // console.log(user);
 
   const theme = useTheme();
 
   // eslint-disable-next-line consistent-return
-  const handleSignUp = () => {
+  const handleSignUp = async () => {
     if (!password || !passwordConfirmation) {
       return Alert.alert('Password is required');
     }
@@ -46,11 +48,19 @@ const SignUpStepTwo = () => {
       return Alert.alert('Passwords do not match');
     }
 
-    // sign up the user
-    navigate('Success', {
-      title: 'Success!',
-      message: 'Now you just need\nto log in',
-      nextScreenRoute: 'SignIn',
+    await api.post('users', {
+      name: user.name,
+      email: user.email,
+      driver_license: user.driversLicense,
+      password,
+    }).then(() => {
+      navigate('Success', {
+        title: 'Success!',
+        message: 'Now you just need\nto log in',
+        nextScreenRoute: 'SignIn',
+      });
+    }).catch(() => {
+      Alert.alert('Error', 'Sign up failed. Please try again later!');
     });
   };
 
