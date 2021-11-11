71c2df674c39c03dc698d095247a93a02878b203
Marcos L <marcosrlj@outlook.com>
2021-11-11 16:48:24 -0300

    configuracao do banco no ios
    fixed watermelondb (dont know how just worked this time. being a dev is
    so crazy)

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin']
+    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
+    plugins: [
+      '@babel/plugin-proposal-decorators', { 'legacy': true },
+      'react-native-reanimated/plugin'
+    ]
   };
 };
---
+++ ios/wmelon.swift
@@ -0,0 +1,8 @@
+//
+//  wmelon.swift
+//  rentx
+//
+//  Created by Marcos L on 11/11/21.
+//
+
+import Foundation
