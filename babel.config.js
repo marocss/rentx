73cd9b04525fa68a90dc70de6ab8a3d0bf6ca9b4
Marcos L <marcosrlj@outlook.com>
2021-11-12 23:09:26 -0300

    configuracao do banco no android

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,12 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
+    presets: [
+      'babel-preset-expo', 
+      // 'module:metro-react-native-babel-preset' // this gives error on react native reanimation (dont know why dont ask me took me a lot of time just to figure out the error)
+    ],
     plugins: [
-      '@babel/plugin-proposal-decorators', { 'legacy': true },
+      ['@babel/plugin-proposal-decorators', { 'legacy': true }],
       'react-native-reanimated/plugin'
     ]
   };
